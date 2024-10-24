ee8faa8e0f74516f3f89f1e7c2f638b1d25a8164
PrisD <prisciladellavecchia@gmail.com>
2024-10-24 03:05:54 -0300

    GetCurso by id

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -9,6 +9,7 @@ import { CursoService } from '../../services/curso.service';
 })
 export class CursosComponent {
   cursos: Curso[] = [];
+  selectedCurso: any;
 
   constructor(private cursoService: CursoService) { }
 
@@ -22,4 +23,9 @@ export class CursosComponent {
     });
   }
 
+  selectCurso(id: number): void {
+    this.cursoService.getCurso(id).subscribe((curso: Curso) => {
+      this.selectedCurso = curso;
+    });
+  }
 }
